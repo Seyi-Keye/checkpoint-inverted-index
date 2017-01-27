915159fa6f7faa44d2d8edbf5bd4786e81c66013
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2017-01-27 02:04:57 +0100

    set development environment and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,44 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const karma = require('karma').Server;
+const path = require('path');
+const rename = require('gulp-rename');
+const browserify = require('gulp-browserify');
 
-gulp.task('default', function(cb) {
-  // place code for your default task here
-  cb();
+gulp.task('default', ['serve', 'watch', 'scripts']);
+
+gulp.task('serve', ()=> {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "./src/frontend/index.html",
+        },
+        port: 8000
+    });
 });
+
+gulp.task('karma', (done) => {
+ karma.start({
+   configFile: path.resolve('karma.conf.js'),
+   singleRun: true
+ }, () => {
+   done();
+ });
+});
+
+gulp.task('scripts', () => {
+gulp.src('jasmine/spec/inverted-index-test.js')
+  .pipe(browserify())
+  .pipe(rename('bundle.js'))
+  .pipe(gulp.dest('jasmine/build'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('*.html',browserSync.reload);
+    gulp.watch('frontend/js/*.js',browserSync.reload);
+});
+
+
+gulp.task('test',['karma']);
+
+
