9c2f99a00efe1ef5ed7f8a46c98f3bd467721bf2
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2017-02-01 00:39:23 +0100

    add JS Doc

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,14 +1,22 @@
 /**
- *
+ * InvertedIndex class with constructor
  */
 class InvertedIndex {
+  /**
+   * class constructor
+   * @constructor
+   */
   constructor() {
     this.allFiles = {};
     this.index = {};
     this.docNumber = {};
   }
   /**
-   *
+   * Create index
+   * @function
+   * @param {Array} file
+   * @param {string} fileName Name of the file being indexed
+   * @return {string} Index created or not
    */
   createIndex(file, fileName) {
     this.index = {}; // initialize empty object to empty this.index in the constructor
@@ -33,7 +41,11 @@ class InvertedIndex {
     return 'Index not created';
   }
   /**
-   *
+   * Assign Index
+   * @function
+   * @param {Array} item unique item to be indexed
+   * @param {Array} docID file number
+   * @return {void}
    */
   assignIndex(item, docID) {
     item.forEach((item) => {
@@ -45,13 +57,18 @@ class InvertedIndex {
     });
   }
   /**
-   *
+   * gets the Index
+   * @param {string} fileName fileName is the name of the file to be searched
+   * @return {object} all unique words in document with correct index
    */
   getIndex(fileName) {
     return this.allFiles[fileName];
   }
   /**
-   *
+   * Validate json Array
+   * @function
+   * @param {object} jsonArray
+   * @return {Boolean} true or false
    */
   isValidJson(jsonArray) { // should be static and should be in a different class e.g helper.js
     if (typeof jsonArray !== 'object' || jsonArray.length === 0) {
@@ -70,14 +87,19 @@ class InvertedIndex {
     }
   }
   /**
-   *
+   * Static tokenize gets unique word
+   * @function
+   * @param {Array} text
+   * @return {Array} lowercase of unique words
    */
   static tokenize(text) {
-    // Utility method
     return text.toLowerCase().match(/\w+/g);
   }
   /**
-   *
+   * searchIndex searches through created index
+   * @param {string} fileName name of file to be searched
+   * @param {string} query the set of input string to be searched
+   * @return {object} searchResult
    */
   searchIndex(fileName, query) {
     let searchQuery = [];
